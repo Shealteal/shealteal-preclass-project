0629ece12cb09e8b93389857457389c53f6127f9
Shealteal <shealteallaurente@gmail.com>
2023-02-22 16:50:01 +0800

    concat name using hash

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,15 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+#
+#
+def ages(nameSample)
+  names = { firstName: "Shealteal", MiddleName: "M",LastName: "Laurente" }
+  concatName = ""
+  names.each do |name , value|
+    concatName += value + ' '
+  end
+  return concatName
+end
+
+p ages("Sample")
